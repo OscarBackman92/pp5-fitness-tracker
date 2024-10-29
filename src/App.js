b7c74489e835e88459f97513040f5e02d52f8898
OscarBackman92 <jan.oscar.backman@gmail.com>
2024-10-29 07:30:33 +0100

    Fix: Add routes back

--- src/App.js
+++ src/App.js
@@ -7,27 +7,99 @@ import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import { AuthProvider } from './context/AuthContext';
 import { WorkoutProvider } from './context/WorkoutContext';
+import { ProfileProvider } from './context/ProfileContext';
+import PrivateRoute from './components/PrivateRoute';
+import Profile from './pages/Profile';
+import Settings from './pages/Settings';
+import WorkoutList from './pages/WorkoutList';
+import LogWorkout from './pages/LogWorkout';
+import EditWorkout from './pages/EditWorkout';
+import WorkoutDetails from './pages/WorkoutDetails';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <Router>
       <AuthProvider>
-        <WorkoutProvider>
-          <div className="App">
-            <Navbar />
-            <main className="content">
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-              </Routes>
-            </main>
-          </div>
-        </WorkoutProvider>
+        <ProfileProvider>
+          <WorkoutProvider>
+            <div className="app-container d-flex flex-column min-vh-100">
+              <Navbar />
+              <main className="flex-grow-1">
+                <Routes>
+                  {/* Public Routes */}
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/register" element={<Register />} />
+
+                  {/* Protected Routes */}
+                  <Route
+                    path="/dashboard"
+                    element={
+                      <PrivateRoute>
+                        <Dashboard />
+                      </PrivateRoute>
+                    }
+                  />
+                  <Route
+                    path="/profile"
+                    element={
+                      <PrivateRoute>
+                        <Profile />
+                      </PrivateRoute>
+                    }
+                  />
+                  <Route
+                    path="/settings"
+                    element={
+                      <PrivateRoute>
+                        <Settings />
+                      </PrivateRoute>
+                    }
+                  />
+                  <Route
+                    path="/workouts"
+                    element={
+                      <PrivateRoute>
+                        <WorkoutList />
+                      </PrivateRoute>
+                    }
+                  />
+                  <Route
+                    path="/workouts/new"
+                    element={
+                      <PrivateRoute>
+                        <LogWorkout />
+                      </PrivateRoute>
+                    }
+                  />
+                  <Route
+                    path="/workouts/edit/:id"
+                    element={
+                      <PrivateRoute>
+                        <EditWorkout />
+                      </PrivateRoute>
+                    }
+                  />
+                  <Route
+                    path="/workouts/:id"
+                    element={
+                      <PrivateRoute>
+                        <WorkoutDetails />
+                      </PrivateRoute>
+                    }
+                  />
+                </Routes>
+              </main>
+              <Footer />
+            </div>
+          </WorkoutProvider>
+        </ProfileProvider>
       </AuthProvider>
     </Router>
   );
 }
+
 
 export default App;
